9f3fe36fd82c5ae8f32b6359e7e7b1382f00c9e9
damico <jd.comment@gmail.com>
2012-05-10 16:39:07 -0400

    Added an OTP window validation test unit

--- src/org/jdamico/tfa/oath/test/TestTotp.java
+++ src/org/jdamico/tfa/oath/test/TestTotp.java
@@ -1,10 +1,13 @@
 package org.jdamico.tfa.oath.test;
 
-import static org.junit.Assert.*;
-
-import java.text.DateFormat;
+import java.lang.reflect.UndeclaredThrowableException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import org.jdamico.tfa.oath.totp.TotpImpl;
 import org.junit.Before;
@@ -24,33 +27,71 @@ public class TestTotp {
 		seed	 = new byte[]{10,20,30,40,60,80,90,00,10,20,30,40,60,80,90,00};
 		t0		 = 0;
 		x		 = 30;
-		unixTime = 59;//System.currentTimeMillis() / 1000L;
+		unixTime = System.currentTimeMillis() / 1000L;
 		steps	 = "0";
 		size	 = 6;
 	}
 
 	@Test
 	public void test() {
-		
 
 		try{
-
 				long T = (unixTime - t0)/x;
 				steps = Long.toHexString(T).toUpperCase();
 				while(steps.length() < 16) steps = "0" + steps;
 				
-
 				System.out.println(TotpImpl.getInstance().generateTOTP(seed, steps, size, "HmacSHA1") + "\t SHA1");
-
 				System.out.println(TotpImpl.getInstance().generateTOTP(seed, steps, size, "HmacSHA256") + "\t SHA256");
-
 				System.out.println(TotpImpl.getInstance().generateTOTP(seed, steps, size, "HmacSHA512") + "\t SHA512");
 
-
-			
 		}catch (final Exception e){
 			System.out.println("Error : " + e);
 		}
 	}
 
+	
+	@Test
+	public void TestOtpWindow(){	
+		
+		int windowMinutes = 5;
+		
+		Map<String,Long> otpWindowMap = new HashMap<String, Long>();
+		long backwardWindow = unixTime - (x*windowMinutes);
+		long forwardWindow =  unixTime + (x*windowMinutes);
+		otpWindowMap.put(genOtp(unixTime, size, seed),unixTime);
+		for (long i = backwardWindow; i <= forwardWindow; i++) otpWindowMap.put(genOtp(i, size, seed),i);
+		
+		Iterator<String> iter = otpWindowMap.keySet().iterator();
+		while(iter.hasNext()){
+			String otp = iter.next();
+			Date date = new Date(otpWindowMap.get(otp) * 1000);
+			SimpleDateFormat formatter = new SimpleDateFormat("[MM/dd/yyyy - H:m:s] ");
+			String strTime = formatter.format(date);
+			System.out.println(strTime+otp);
+		}
+	}
+	
+	private String genOtp(long baseTime, int size, byte[] seed) {
+		String otpGen = null;
+		String steps	 = "0";
+		long t0		 = 0;
+		long x		 = 30;
+		long T = (baseTime - t0)/x;
+		steps = Long.toHexString(T).toUpperCase();
+		while(steps.length() < 16) steps = "0" + steps;
+		
+		try {
+			otpGen = TotpImpl.getInstance().generateTOTP(seed, steps, size, "HmacSHA1");
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (UndeclaredThrowableException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		
+		
+		return otpGen;
+	}
+	
 }
